966654442d5fb78904c38c14fc8a79a73fd33f43
Rafał Potorski <hej_rafal@wp.pl>
2020-07-31 00:10:23 +0200

    init page

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,14 +1,9 @@
-/*
- * Welcome to your app's main JavaScript file!
- *
- * We recommend including the built version of this JavaScript file
- * (and its CSS file) in your base layout (base.html.twig).
- */
-
-// any CSS you import will output into a single css file (app.css in this case)
 import '../css/app.css';
 
-// Need jQuery? Install it with "yarn add jquery", then uncomment to import it.
-// import $ from 'jquery';
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-console.log('Hello Webpack Encore! Edit me in assets/js/app.js');
+//const el = React.createElement('h2', null, 'Hakuna matata');
+const el = <h2>Hakuna matata</h2>
+
+ReactDOM.render(el, document.getElementById('test'));
---
+++ src/Controller/MainController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class MainController extends AbstractController
+{
+    /**
+     * @Route("/", name="homepage")
+     */
+    public function index()
+    {
+        return $this->render('main/index.html.twig');
+    }
+}
